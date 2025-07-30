1c1722cbc9cb23fde888c2c585859a4ff4f253ea
Li-Mateus <mateushardware@hotmail.com>
2025-07-30 16:13:30 -0300

    Criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({style: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
+// function images() {
+//     return gulp.src('./src/images/**/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('./src/images/minified'));
+// } -------> função para minificar imagens
+
 exports.default = styles;
+
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(styles));
 }
