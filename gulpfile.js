67a9b021d3c7e06a031ac02cf86f867c32d8f2af
Li-Mateus <mateushardware@hotmail.com>
2025-08-04 11:13:45 -0300

    Configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+// const imagemin = require('gulp-imagemin');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() {
     return gulp.src('./src/styles/*scss')
@@ -14,8 +21,9 @@ function styles() {
 //         .pipe(gulp.dest('./src/images/minified'));
 // } -------> função para minificar imagens
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function() {
-    gulp.watch('./src/styles/*scss', gulp.parallel(styles));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
